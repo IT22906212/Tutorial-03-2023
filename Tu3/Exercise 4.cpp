d0a5ddf76e8a3741d429a954f30e541874ae8c01
IT22906212 <127375075+IT22906212@users.noreply.github.com>
2023-03-23 06:26:12 +0000

    tute3

---
+++ Tu3/Exercise 4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+void print(int len, int wth);
+
+int main() 
+{
+ int length,width,tital;
+ print(10,5);
+}
+void print(int len, int wth) 
+{
+ 	int total;
+ 	cout<<"length:";
+	cin>>len;
+	
+	cout<<"Width:";
+	cin>>wth; 
+	
+	cout<<"Width:"<<wth;
+	  
+}
